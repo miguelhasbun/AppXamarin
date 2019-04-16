9afacdb6c77afeb4d27eb09c3ed65428ee1473ce
miguelhasbun <miguelneptaly@gmail.com>
2019-04-15 21:15:20 -0700

    commit inicial

---
+++ Prueba/Prueba/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prueba
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            btnInicioSesion.Clicked += btnInicioSesion_Clicked;
+            btnSalir.Clicked += btnSalir_Clicked;
+        }
+        private void btnInicioSesion_Clicked(object sender, EventArgs e)
+        {
+
+            if (txtuser.Text=="ASH" && txtcontra.Text == "PIKACHU")
+            {
+                ((NavigationPage)this.Parent).PushAsync(new MasterPokemon());
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario y/o contraseña incorrecta", "OK");
+            }
+        }
+
+        async void btnSalir_Clicked(object sender, EventArgs e)
+        {
+
+            bool answer = await DisplayAlert("Advertencia", "¿Quiere salir de la app?", "Si", "No");
+            if(answer)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+            }
+        }
+    }
+}
---
+++ Prueba/Prueba/MasterPokemon.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prueba
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPokemon : ContentPage
+	{
+		public MasterPokemon ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+            btnend.Clicked += btnSalir_Clicked;
+        }
+        async void btnSalir_Clicked(object sender, EventArgs e)
+        {
+
+            bool answer = await DisplayAlert("ASH", "¿Quiere cerrar su sesión activa?", "Si", "No");
+            if (answer)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+            }
+        }
+    }
+}
